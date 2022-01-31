12f098d284c7b75e66d76c51fe8202b694e7ba59
Horacio <87150874+horacio507@users.noreply.github.com>
2022-01-31 19:57:16 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@
 # Short-Description: Something
 # Description:       Something else
 ### END INIT INFO
-cd /home/horacio/Dropbox/RemoteTurnOff
+cd /home/tu_nombre_de_usuario/Dropbox/RemoteTurnOff
 inotifywait -e close_write,moved_to,create -m . |
 while read -r directory events filename; do
   if [ "$filename" = "fileToWatch.txt" ]; then
